6e4978a1d1f0a11436d58390ceb1a9d2e4141bf8
Manoj Duraisamy <manoj.duraisamy@aspiresys.kl>
2025-04-07 14:01:06 +0530

    login security api part completed

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,5 @@
 export const ENDPOINTS = {
   LOGIN: "api/customers/login",
   GET_ORDER_BY_ID: "api/order/",
+  GET_CUSTOMER_INFO_BY_ID: "api/customers/",
 };
--- src/types/customer.ts
+++ src/types/customer.ts
@@ -7,3 +7,11 @@ export interface LoginResponse {
   message: string;
   customerNumber: number;
 }
+
+export interface CustomerResponse {
+  id: string;
+  firstName: string;
+  lastName: string;
+  emailId: string;
+  customerType: string;
+}
